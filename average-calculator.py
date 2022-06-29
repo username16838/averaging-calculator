d51db0cc17348e8f3a8c9c6471a680da86123c30
username16838 <89617918+username16838@users.noreply.github.com>
2022-06-29 08:49:48 -0400

    v 1.2.1
    Added threading.

--- average-calculator.py
+++ average-calculator.py
@@ -1,117 +1,128 @@
 import math
+from threading import Thread
 
-# usr manual
+def main():
+    # usr manual
 
-print("average calculator tool v1.2")
-print("once you have typed the maximum amount of numbers you want to average...")
-num2 = float(input("How many number(s) are you avereging? 2-10\n" + '>>> '))
+    print("average calculator tool v1.2")
+    print("once you have typed the maximum amount of numbers you want to average...")
+    num2 = None
 
-# averaging
+    num2 = float(input("How many number(s) are you avereging? 2-10\n" + '>>> '))
+    
+    print("You may continue with entering data")
 
-if num2 ==float('2'):
-    stock1 = float(input('>>> '))
-    stock2 = float(input('>>> '))
-    num1 = stock1 + stock2
-    sum = num1 / num2
-    print(">>> " + str(sum))
+    # averaging
 
-if num2 ==float('3'):
-    stock1 = float(input('>>> '))
-    stock2 = float(input('>>> '))
-    stock3 = float(input('>>> '))
-    num1 = stock1 + stock2 + stock3
-    sum = num1 / num2
-    print(">>> " + str(sum))
+    if num2 ==float('2'):
+        stock1 = float(input('>>> '))
+        stock2 = float(input('>>> '))
+        num1 = stock1 + stock2
+        sum = num1 / num2
+        print(">>> " + str(sum))
 
-if num2 ==float('4'):
-    stock1 = float(input('>>> '))
-    stock2 = float(input('>>> '))
-    stock3 = float(input('>>> '))
-    stock4 = float(input('>>> '))
-    num1 = stock1 + stock2 + stock3 + stock4
-    sum = num1 / num2
-    print(">>> " + str(sum))
+    if num2 ==float('3'):
+        stock1 = float(input('>>> '))
+        stock2 = float(input('>>> '))
+        stock3 = float(input('>>> '))
+        num1 = stock1 + stock2 + stock3
+        sum = num1 / num2
+        print(">>> " + str(sum))
 
-if num2 ==float('5'):
-    stock1 = float(input('>>> '))
-    stock2 = float(input('>>> '))
-    stock3 = float(input('>>> '))
-    stock4 = float(input('>>> '))
-    stock5 = float(input('>>> '))
-    num1 = stock1 + stock2 + stock3 + stock4 + stock5
-    sum = num1 / num2
-    print(">>> " + str(sum))
+    if num2 ==float('4'):
+        stock1 = float(input('>>> '))
+        stock2 = float(input('>>> '))
+        stock3 = float(input('>>> '))
+        stock4 = float(input('>>> '))
+        num1 = stock1 + stock2 + stock3 + stock4
+        sum = num1 / num2
+        print(">>> " + str(sum))
 
-if num2 ==float('6'):
-    stock1 = float(input('>>> '))
-    stock2 = float(input('>>> '))
-    stock3 = float(input('>>> '))
-    stock4 = float(input('>>> '))
-    stock5 = float(input('>>> '))
-    stock6 = float(input('>>> '))
-    num1 = stock1 + stock2 + stock3 + stock4 + stock5 + stock6
-    sum = num1 / num2
-    print(">>> " + str(sum))
+    if num2 ==float('5'):
+        stock1 = float(input('>>> '))
+        stock2 = float(input('>>> '))
+        stock3 = float(input('>>> '))
+        stock4 = float(input('>>> '))
+        stock5 = float(input('>>> '))
+        num1 = stock1 + stock2 + stock3 + stock4 + stock5
+        sum = num1 / num2
+        print(">>> " + str(sum))
 
-if num2 ==float('7'):
-    stock1 = float(input('>>> '))
-    stock2 = float(input('>>> '))
-    stock3 = float(input('>>> '))
-    stock4 = float(input('>>> '))
-    stock5 = float(input('>>> '))
-    stock6 = float(input('>>> '))
-    stock7 = float(input('>>> '))
-    num1 = stock1 + stock2 + stock3 + stock4 + stock5 + stock6 + stock7
-    sum = num1 / num2
-    print(">>> " + str(sum))
+    if num2 ==float('6'):
+        stock1 = float(input('>>> '))
+        stock2 = float(input('>>> '))
+        stock3 = float(input('>>> '))
+        stock4 = float(input('>>> '))
+        stock5 = float(input('>>> '))
+        stock6 = float(input('>>> '))
+        num1 = stock1 + stock2 + stock3 + stock4 + stock5 + stock6
+        sum = num1 / num2
+        print(">>> " + str(sum))
 
-if num2 ==float('8'):
-    stock1 = float(input('>>> '))
-    stock2 = float(input('>>> '))
-    stock3 = float(input('>>> '))
-    stock4 = float(input('>>> '))
-    stock5 = float(input('>>> '))
-    stock6 = float(input('>>> '))
-    stock7 = float(input('>>> '))
-    stock8 = float(input('>>> '))
-    num1 = stock1 + stock2 + stock3 + stock4 + stock5 + stock6 + stock7 + stock8
-    sum = num1 / num2
-    print(">>> " + str(sum))
+    if num2 ==float('7'):
+        stock1 = float(input('>>> '))
+        stock2 = float(input('>>> '))
+        stock3 = float(input('>>> '))
+        stock4 = float(input('>>> '))
+        stock5 = float(input('>>> '))
+        stock6 = float(input('>>> '))
+        stock7 = float(input('>>> '))
+        num1 = stock1 + stock2 + stock3 + stock4 + stock5 + stock6 + stock7
+        sum = num1 / num2
+        print(">>> " + str(sum))
 
-if num2 ==float('9'):
-    stock1 = float(input('>>> '))
-    stock2 = float(input('>>> '))
-    stock3 = float(input('>>> '))
-    stock4 = float(input('>>> '))
-    stock5 = float(input('>>> '))
-    stock6 = float(input('>>> '))
-    stock7 = float(input('>>> '))
-    stock8 = float(input('>>> '))
-    stock9 = float(input('>>> '))
-    num1 = stock1 + stock2 + stock3 + stock4 + stock5 + stock6 + stock7 + stock8 + stock9
-    sum = num1 / num2
-    print(">>> " + str(sum))
+    if num2 ==float('8'):
+        stock1 = float(input('>>> '))
+        stock2 = float(input('>>> '))
+        stock3 = float(input('>>> '))
+        stock4 = float(input('>>> '))
+        stock5 = float(input('>>> '))
+        stock6 = float(input('>>> '))
+        stock7 = float(input('>>> '))
+        stock8 = float(input('>>> '))
+        num1 = stock1 + stock2 + stock3 + stock4 + stock5 + stock6 + stock7 + stock8
+        sum = num1 / num2
+        print(">>> " + str(sum))
 
-if num2 ==float('10'):
-    stock1 = float(input('>>> '))
-    stock2 = float(input('>>> '))
-    stock3 = float(input('>>> '))
-    stock4 = float(input('>>> '))
-    stock5 = float(input('>>> '))
-    stock6 = float(input('>>> '))
-    stock7 = float(input('>>> '))
-    stock8 = float(input('>>> '))
-    stock9 = float(input('>>> '))
-    stock10 = float(input('>>> '))
-    num1 = stock1 + stock2 + stock3 + stock4 + stock5 + stock6 + stock7 + stock8 + stock9 + stock10
-    sum = num1 / num2
-    print(">>> " + str(sum))
+    if num2 ==float('9'):
+        stock1 = float(input('>>> '))
+        stock2 = float(input('>>> '))
+        stock3 = float(input('>>> '))
+        stock4 = float(input('>>> '))
+        stock5 = float(input('>>> '))
+        stock6 = float(input('>>> '))
+        stock7 = float(input('>>> '))
+        stock8 = float(input('>>> '))
+        stock9 = float(input('>>> '))
+        num1 = stock1 + stock2 + stock3 + stock4 + stock5 + stock6 + stock7 + stock8 + stock9
+        sum = num1 / num2
+        print(">>> " + str(sum))
 
-print("would you like to round you answer?  y/n")
-rond = input('>>> ')
+    if num2 ==float('10'):
+        stock1 = float(input('>>> '))
+        stock2 = float(input('>>> '))
+        stock3 = float(input('>>> '))
+        stock4 = float(input('>>> '))
+        stock5 = float(input('>>> '))
+        stock6 = float(input('>>> '))
+        stock7 = float(input('>>> '))
+        stock8 = float(input('>>> '))
+        stock9 = float(input('>>> '))
+        stock10 = float(input('>>> '))
+        num1 = stock1 + stock2 + stock3 + stock4 + stock5 + stock6 + stock7 + stock8 + stock9 + stock10
+        sum = num1 / num2
+        print(">>> " + str(sum))
 
-if rond =='y':
-    print(round(sum))
-else:
-    print('ending')
+    print("would you like to round you answer?  y/n")
+    rond = input('>>> ')
 
+    if rond =='y':
+        print(round(sum))
+    else:
+        print('ending')
+
+# start/end Threads
+
+t1 = Thread(target=main)
+t1.start()
+t1.join()
